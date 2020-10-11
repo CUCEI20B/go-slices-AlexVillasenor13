24ceea63373f6f4ba646a85f4b24f405f80032de
AlexVillasenor1 <isaac.villasenor2923@alumnos.udg.mx>
2020-10-11 21:07:26 +0000

    Se crea el slide a partir de el primer entero, y se suman los enteros

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
+  
 package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main(){
+	var n int
+    var total int = 0
+	fmt.Scan(&n)
+    s := make([]int, 0, n)
+	for i:=0; i<n; i++{
+        var aux int
+        fmt.Scan(&aux)
+        total += aux
+        s = append(s, aux)
+	}
+
+	fmt.Print(total)
 }
